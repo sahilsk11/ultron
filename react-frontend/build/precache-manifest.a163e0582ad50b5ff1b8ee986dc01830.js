a795e701ab2143687df619d8e11c97277c863c24
Sahil Kapur <sahilkapur.a@gmail.com>
2020-07-16 06:37:01 -0700

    fix routes

---
+++ react-frontend/build/precache-manifest.a163e0582ad50b5ff1b8ee986dc01830.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61a3ac88ba5e069bf9366bd99004a5c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd39b4d04d165b0a1e18",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2401aca8bf2836c553eb",
+    "url": "/static/js/2.0df453a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0df453a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd39b4d04d165b0a1e18",
+    "url": "/static/js/main.2dcd5d0a.chunk.js"
+  },
+  {
+    "revision": "4792b66c7ea268d3b997",
+    "url": "/static/js/runtime-main.d43be887.js"
+  }
+]);
