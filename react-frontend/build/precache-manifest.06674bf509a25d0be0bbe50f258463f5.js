c1cc0bbd5ef404903407d0340169a3a3ebdae9e7
Sahil Kapur <sahilkapur.a@gmail.com>
2020-07-16 06:20:28 -0700

    add react build

---
+++ react-frontend/build/precache-manifest.06674bf509a25d0be0bbe50f258463f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f755585cd564542733ad112edac7e9a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb69579f6a5fb48bba66",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2401aca8bf2836c553eb",
+    "url": "/static/js/2.0df453a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0df453a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb69579f6a5fb48bba66",
+    "url": "/static/js/main.97da7665.chunk.js"
+  },
+  {
+    "revision": "4792b66c7ea268d3b997",
+    "url": "/static/js/runtime-main.d43be887.js"
+  }
+]);
