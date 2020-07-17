3cf9b6e3eb14d3701ed97ef075bb42dca075c9be
Sahil Kapur <sahilkapur.a@gmail.com>
2020-07-16 20:59:01 -0700

    upgrade UI

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+import "./App.css"
 
 function App() {
   const { listening, transcript, resetTranscript } = useSpeechRecognition();
-  const onClickFunction = listening ? SpeechRecognition.stopListening : () => SpeechRecognition.startListening({ continuous: true });
-  const send = () => {
+  const send = async () => {
     const simulateProd = true;
     const endpoint = simulateProd || process.env.NODE_ENV === "production" ? "https://api.sahilkapur.com/addDailyWeight" : "http://localhost:8080/addDailyWeight";
     const params = "?transcript="+transcript.toLowerCase();
@@ -12,12 +12,22 @@ function App() {
       .then(response => response.json())
       .then(data => alert(data));
   }
+  useEffect(() => {
+    if (!listening && transcript !=='') {
+      send()
+    }
+  }, [listening]);
   return (
     <div>
-      <button onClick={onClickFunction}>start</button>
+      {/* <button onClick={onClickFunction}>start</button>
       <button onClick={resetTranscript}>reset</button>
-      <button onClick={send}>send</button>
-      <p>{transcript}</p>
+      <button onClick={send}>send</button> */}
+      <h2 className="greeting">Good evening, Sahil.</h2>
+      <img className="voice-icon" onClick={()=>SpeechRecognition.startListening({continuous: false})}
+      src="https://cdn.dribbble.com/users/32512/screenshots/5668419/calm_ai_design_by_gleb.gif" />
+      <p className="transcript"><em>{transcript}</em></p>
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet" />
+
     </div>
   );
 }
