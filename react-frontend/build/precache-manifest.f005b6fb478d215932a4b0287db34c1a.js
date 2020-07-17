726be56b10f8172a034c340b86ce6b19f254f381
Sahil Kapur <sahilkapur.a@gmail.com>
2020-07-16 20:59:06 -0700

    upgrade UI

---
+++ react-frontend/build/precache-manifest.f005b6fb478d215932a4b0287db34c1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6d91ae296263aaea6b3254b6fd17176",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7eaf80115e1ab74511c",
+    "url": "/static/css/main.71b26812.chunk.css"
+  },
+  {
+    "revision": "8c14371466e184bd6c16",
+    "url": "/static/js/2.32abb18a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.32abb18a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7eaf80115e1ab74511c",
+    "url": "/static/js/main.14ff7dc2.chunk.js"
+  },
+  {
+    "revision": "4792b66c7ea268d3b997",
+    "url": "/static/js/runtime-main.d43be887.js"
+  }
+]);
