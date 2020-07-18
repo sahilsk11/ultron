5008d31ccd0e7867c8a0fb16dd2a5bb545ee132e
Sahil Kapur <sahilkapur.a@gmail.com>
2020-07-18 02:47:23 -0700

    add frontent

---
+++ react-frontend/build/precache-manifest.1eb5ff506969591b0b1c3cbcbf0210f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "980fe699ac0e7d0348f65c379dbac52e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a401ea0077bc9a2fd1e6",
+    "url": "/static/css/main.7c2dc398.chunk.css"
+  },
+  {
+    "revision": "54485e330e0525e7d4d8",
+    "url": "/static/js/2.d08f9d7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d08f9d7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a401ea0077bc9a2fd1e6",
+    "url": "/static/js/main.5e7af590.chunk.js"
+  },
+  {
+    "revision": "4792b66c7ea268d3b997",
+    "url": "/static/js/runtime-main.d43be887.js"
+  }
+]);
