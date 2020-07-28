ecd033100c5af0140d72ac609576ea8020d90da3
Sahil Kapur <sahilkapur.a@gmail.com>
2020-07-27 23:27:11 -0700

    fix screen

---
+++ react-frontend/build/precache-manifest.f48fd7f804732186ec270cf5c2ed2c17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66d929795893dbff8b25949ab51d82e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a06a0c357132d07ba60",
+    "url": "/static/css/main.87afcec3.chunk.css"
+  },
+  {
+    "revision": "dd143cd39c41032cee23",
+    "url": "/static/js/2.da043f30.chunk.js"
+  },
+  {
+    "revision": "e46fc7d39878ee64e7b083a156855138",
+    "url": "/static/js/2.da043f30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a06a0c357132d07ba60",
+    "url": "/static/js/main.d65c8cf5.chunk.js"
+  },
+  {
+    "revision": "4792b66c7ea268d3b997",
+    "url": "/static/js/runtime-main.d43be887.js"
+  }
+]);
