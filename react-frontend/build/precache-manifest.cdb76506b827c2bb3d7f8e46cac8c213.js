01a57d1b21a75eb784a9d74e53db41972f88a5bd
Sahil Kapur <sahilkapur.a@gmail.com>
2020-07-28 17:46:24 -0700

    add text to speech

---
+++ react-frontend/build/precache-manifest.cdb76506b827c2bb3d7f8e46cac8c213.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beeadcfa1468e817ed375bc0a31a2e2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43cad925aa9c0023b1d5",
+    "url": "/static/css/main.87afcec3.chunk.css"
+  },
+  {
+    "revision": "438ca917535438ddc0ca",
+    "url": "/static/js/2.aa75472e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aa75472e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43cad925aa9c0023b1d5",
+    "url": "/static/js/main.30425c0c.chunk.js"
+  },
+  {
+    "revision": "4792b66c7ea268d3b997",
+    "url": "/static/js/runtime-main.d43be887.js"
+  }
+]);
