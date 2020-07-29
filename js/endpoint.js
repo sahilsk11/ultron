7a94ab4a05af271488b79603f2ec803bb74d1d9e
Sahil Kapur <sahilkapur.a@gmail.com>
2020-07-29 00:41:33 -0700

    fix tts

--- js/endpoint.js
+++ js/endpoint.js
@@ -39,7 +39,7 @@ app.get("/setIntent", async (req, res) => {
   console.log(intent);
   const body = await speechEngine.intentRouter({ intent, transcript });
   const fileName = generateFileName() + ".wav";
-  exec(`./mimic -t "${body.message}" --setf duration_stretch=1.1 --setf int_f0_target_mean=90 -o audio/${fileName}`, (error, stdout, stderr) => {
+  exec("./mimic -t " + body.message + " --setf duration_stretch=1.1 -o audio/" + fileName, (error, stdout, stderr) => {
     res.json({ code: 200, intent, ...body, fileName });
     if (error) {
       console.log(`error: ${error.message}`);
@@ -53,10 +53,11 @@ app.get("/setIntent", async (req, res) => {
   });
 });
 
-app.get('/audioFile', function (req, res) {
+app.get('/audioFile', async function async (req, res) {
   const filename = req.query.fileName;
   let sent = false;
-  while (!sent) {
+  let tries = 0;
+  while (!sent && tries++ < 50) {
     try {
       if (fs.existsSync("./audio/" + filename)) {
         sent = true;
@@ -65,8 +66,13 @@ app.get('/audioFile', function (req, res) {
     } catch (err) {
       console.error(err)
     }
+    await sleep(100);
   }
 });
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 function generateFileName() {
   const chars = '0123456789ABCDEFGHIJKLMNOPQRTUVWXYZ';
