cf44f17633985dfdbc3a5b39f73d6c46158ffaf7
Sahil Kapur <sahilkapur.a@gmail.com>
2020-08-05 02:36:13 -0700

    fix datetime

--- js/intents/timeIntent.js
+++ js/intents/timeIntent.js
@@ -1,4 +1,5 @@
 const { Intent } = require("../intent.js");
+const moment = require("moment");
 
 class TimeIntent extends Intent {
   constructor({ transcript }) {
@@ -8,36 +9,30 @@ class TimeIntent extends Intent {
       utterances: ["time", "sunset", "sunrise"],
       intentName: "timeIntent"
     });
-    this.timeZoneOffset = -7;
+    this.timeOffset = -7; //check for timezone change
   }
 
   async execute() {
-    let now = new Date();
-    now = this.utcToPst(now);
-    console.log(now);
-    console.log(now.getHours());
+    const time = this.getCurrentTime(this.timeOffset);
 
-    const timeString = this.constructTimeString(now);
+    const timeString = this.constructTimeString(time);
     const { sunrise, sunset } = await this.getDayInformation();
-    const snarkyMessage = this.constructMessage(now, sunrise, sunset);
-    let message = "It is " + timeString + snarkyMessage;
+    const snarkyMessage = this.constructMessage(time, sunrise, sunset);
+    let message = "It is " + timeString + " " + snarkyMessage;
     return { code: 200, message, intent: this.intentName }
   }
 
-  constructTimeString(now) {
-    let hour = now.getHours();
-    const pm = hour < 12 ? " A.M. " : " P.M. ";
-    hour = hour % 12 === 0 ? 12 : hour % 12;
-    const min = now.getMinutes();
-    return hour + ":" + min + pm;
+  constructTimeString(time) {
+    const hour = time.hour % 12 === 0 ? 12 : time.hour % 12;
+    return hour + ":" + time.minutes + " " + time.timeOfDay;
   }
 
   /**
    * Based on the current time, construct some snarky message
    * @param hour as 0-23 hr time
    */
-  constructMessage(now, sunriseTime, sunsetTime) {
-    const hour = now.getHours();
+  constructMessage(time, sunriseTime, sunsetTime) {
+    const { hour, unixTime} = time;
     if (hour < 4) {
       return "The night, or should I say day, is young, sir.";
     } else if (hour < 6) {
@@ -62,12 +57,15 @@ class TimeIntent extends Intent {
     return { sunrise: new Date(sunrise), sunset: new Date(sunset) };
   }
 
-  utcToPst(d) {
-    d = new Date();
-    console.log(d.getHours())
-    d.setHours(d.getHours() + this.timeZoneOffset);
-    console.log(d.getHours())
-    return d;
+  getCurrentTime(offset) {
+    const now = new moment().utc();
+    return {
+      hour: now.hours() + offset,
+      minutes: now.minutes(),
+      seconds: now.seconds(),
+      timeOfDay: (now.hours() + offset) < 12 ? "A.M." : "P.M.",
+      unixTimeStamp: now.valueOf()
+    }
   }
 }
 
