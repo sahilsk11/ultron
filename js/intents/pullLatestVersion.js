5478700e8f4016e2cc509fec24263cf4e2255bae
Sahil Kapur <sahilkapur.a@gmail.com>
2020-08-08 03:15:44 -0700

    fix async errors

--- js/intents/pullLatestVersion.js
+++ js/intents/pullLatestVersion.js
@@ -25,6 +25,7 @@ class PullLatestVersion extends Intent {
       resolve({ code: 200, message, intent: this.intentName });
     }));
     if (this.isProduction()) this.restartPm2();
+    
     return response;
   }
 
