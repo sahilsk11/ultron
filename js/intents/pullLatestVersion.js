bd9f68a508ce05ff4212347b54af853bf285a5ce
Sahil Kapur <sahilkapur.a@gmail.com>
2020-08-08 03:34:10 -0700

    testing commit change

--- js/intents/pullLatestVersion.js
+++ js/intents/pullLatestVersion.js
@@ -28,6 +28,7 @@ class PullLatestVersion extends Intent {
       resolve({ code: 200, message, intent: this.intentName });
     }));
     if (this.isProduction()) this.restartPm2();
+    
     return response;
   }
 
