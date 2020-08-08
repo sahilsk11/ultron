b695d94d4d6111f89e351e056ffea436e667cca6
Sahil Kapur <sahilkapur.a@gmail.com>
2020-08-08 03:04:21 -0700

    add message

--- js/intents/pullLatestVersion.js
+++ js/intents/pullLatestVersion.js
@@ -2,7 +2,7 @@ const { Intent } = require("../intent.js");
 const { exec } = require("child_process");
 const { resolve } = require("path");
 
-class PullLatestVersion extends Intent { // TO-DO change name
+class PullLatestVersion extends Intent {
   constructor({ transcript }) {
     super({
       transcript,
@@ -13,18 +13,20 @@ class PullLatestVersion extends Intent { // TO-DO change name
   }
 
   async execute() {
-    const command = "git pull;"
+    const command = "git pull; git log -1 --pretty=%B;"
     const response = await new Promise(resolve => exec(command, (error, stdout, stderr) => {
-      const out = stdout;
       let message = "";
-      if (out.includes("Already up-to-date.")) {
+      if (stdout.includes("Already up-to-date.")) {
         message = "Sir, I am already running the latest version."
+      } else {
+        let out = stdout.split("\n");
+        message = "Updated to latest version with update notes: \"" + out[1] + "\"";
       }
-      //console.log("'" + out + "'");
+      console.log("'" + out.split("\n") + "'");
       resolve({ code: 200, message, intent: this.intentName });
     }));
     return response;
   }
 }
 
-module.exports.IntentClass = PullLatestVersion; //TO-DO change name
+module.exports.IntentClass = PullLatestVersion;
