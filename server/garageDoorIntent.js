5c8ee566366fa569ff7b50a8567732cbfdda46a7
Sahil Kapur <sahilkapur.a@gmail.com>
2020-08-10 12:55:11 -0700

    add garage door intent

---
+++ server/garageDoorIntent.js
@@ -0,0 +1,39 @@
+const { Intent } = require("../intent.js");
+const axios = require("axios");
+
+class GarageDoorIntent extends Intent {
+  constructor({ transcript }) {
+    super({
+      transcript,
+      regex: "",
+      utterances: ["garage"],
+      intentName: "garageDoorIntent"
+    });
+  }
+
+  async execute() {
+    let message;
+    if (this.transcript.includes("open")) {
+      this.makeRequest("open");
+      message = "Opening the garage door now.";
+    } else if (this.transcript.includes("close")) {
+      message = "Closing the garage door now.";
+      this.makeRequest("close");
+    }
+    return { code: 200, message, intent: this.intentName }
+  }
+
+  async makeRequest(command) {
+    const endpoint = "http://remote.kapurs.net:14380/GDS/status.py?command=" + command;
+    const username = admin;
+    const password = this.getApiKey("HOME_PASSWORD");
+    const response = await axios.get(endpoint, {
+      auth: {
+        username,
+        password
+      }
+    });
+  }
+}
+
+module.exports.IntentClass = GarageDoorIntent;
