1cddcdd93b94f14041dca727b3900813bdd5a137
Sahil Kapur <sahilkapur.a@gmail.com>
2020-08-10 01:46:46 -0700

    reduce time threshold and reset

---
+++ react-frontend/build/precache-manifest.66e8dc7aa33de8150f36ad587166bc06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49fc39ca18e8e6c12f08698870946001",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d03a34836898874704ec",
+    "url": "/static/css/main.2769f397.chunk.css"
+  },
+  {
+    "revision": "661816d7375507c1781d",
+    "url": "/static/js/2.32653458.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.32653458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d03a34836898874704ec",
+    "url": "/static/js/main.a112cfa9.chunk.js"
+  },
+  {
+    "revision": "4792b66c7ea268d3b997",
+    "url": "/static/js/runtime-main.d43be887.js"
+  }
+]);
