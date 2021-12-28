bda54962e7d7f87dee50a256ac7b114e4edd814c
sahilsk11 <sahilkapur.a@gmail.com>
2021-12-27 18:46:21 -0800

    add berry light controls

--- server/intentLibary/intents/lightsIntent.js
+++ server/intentLibary/intents/lightsIntent.js
@@ -8,29 +8,38 @@ class LightsIntent extends Intent {
       transcript,
       regex: [],
       utterances: [
-        "lights"
+        "lights", "light"
       ],
       intentName: "lightsIntent"
     });
   }
 
+
   async execute() {
+    var currentHome = "berry"; // can be berry or bentley
+
     this.transcript = this.transcript.replace("turn ", "");
     const lightIndex = this.transcript.indexOf("light");
     const roomEndIndex = lightIndex - 1;
     const roomStartIndex = 0;
     const roomName = this.transcript.substring(roomStartIndex, roomEndIndex);
 
+    // command should either be ON or OFF
     const commandStartIndex = this.indexOfNextSpace(lightIndex, this.transcript) + 1;
     const commandEndIndex = this.indexOfNextSpace(commandStartIndex, this.transcript);
     const commandName = this.transcript.substring(commandStartIndex, commandEndIndex);
 
-    const responseCode = await this.controlLights({ roomName, commandName });
+    let responseCode;
+    if (currentHome === "berry") {
+      responseCode = await this.controlBerryLights({ roomName, commandName });
+    } else {
+      responseCode = await this.controlLights({ roomName, commandName });
+    }
     let message;
     if (responseCode === 200) {
       message = "Turning " + roomName + " lights " + commandName;
     } else {
-      message =  "Could not turn " + roomName + " lights " + commandName;
+      message = "Could not turn " + roomName + " lights " + commandName;
     }
     return { code: 200, message, intent: this.intentName }
   }
@@ -46,9 +55,37 @@ class LightsIntent extends Intent {
     const entity_id = "light." + roomName.replace(" ", "_");
     const data = { entity_id }
     const url = "http://remote.kapurs.net:8123/api/services/light/turn_" + commandName;
-    
+
     const response = await axios.post(url, data, config);
     return response.status;
+  }
+
+  async controlBerryLights({roomName, commandName}) {
+    const apiKey = process.env.KNOX_KEY;
+    const config = {
+      headers: {
+        'Authorization': 'Bearer ' + apiKey,
+        'Content-Type': 'application/json'
+      }
+    }
+
+    roomName = correctBerryRoomName(roomName);
+
+    requestBody = {
+      "lightName": roomName.toUpperCase(),
+      "state": commandName.toUpperCase()
+    }
+
+    const response = await axios.post("http://localhost:8000", data, config);
+    return response.status;
+  }
+
+  correctBerryRoomName(roomName) {
+    roomName = roomName.replace("lights", "")
+    if (roomName.indexOf("bedroom") >= 0) {
+      return "SAHIL_BEDROOM"
+    }
+    return roomName;
   }
 }
 
