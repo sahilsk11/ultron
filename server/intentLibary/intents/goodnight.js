bb850a7901de86ed7b4670e8038558981e0e3303
sahilsk11 <sahilkapur.a@gmail.com>
2021-12-27 18:20:40 -0800

    update auth

--- server/intentLibary/intents/goodnight.js
+++ server/intentLibary/intents/goodnight.js
@@ -13,10 +13,17 @@ class Goodnight extends Intent {
   }
 
   async execute() {
+    const apiKey = process.env.KNOX_KEY;
+    const config = {
+      headers: {
+        'Authorization': 'Bearer ' + apiKey,
+        'Content-Type': 'application/json'
+      }
+    }
     let message = "Goodnight, sir. Lighting the way up.";
     await axios.post(
-      "http://localhost:8000/goodnight",
-      {}
+      "http://localhost:8000/scenes/goodnight",
+      {}, config
     )
     return { code: 200, message, intent: this.intentName }
   }
